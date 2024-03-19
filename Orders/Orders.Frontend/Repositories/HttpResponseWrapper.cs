7de85c187ab234857eee0da73b363fb62f7f8c15
Julian <jhurtadot01@outlook.com>
2024-03-19 19:29:25 -0400

    Begin Repopsitory Pattern

---
+++ Orders/Orders.Frontend/Repositories/HttpResponseWrapper.cs
@@ -0,0 +1,46 @@
+﻿using System.Net;
+
+namespace Orders.Frontend.Repositories
+
+{
+    public class HttpResponseWrapper<T>
+    {
+        public HttpResponseWrapper(T? response, bool error, HttpResponseMessage httpResponseMessage)
+        {
+            Response = response;
+            Error = error;
+            HttpResponseMessage = httpResponseMessage;
+        }
+
+        public T? Response { get; }
+        public bool Error { get; }
+        public HttpResponseMessage HttpResponseMessage { get; }
+
+        public async Task<string?> GetErrorMessageAsync()
+        {
+            if (!Error)
+            {
+                return null;
+            }
+            var statusCode = HttpResponseMessage.StatusCode;
+            if (statusCode == HttpStatusCode.NotFound)
+            {
+                return "Recurso no encontrado";
+            }
+            if (statusCode == HttpStatusCode.BadRequest)
+            {
+                return await HttpResponseMessage.Content.ReadAsStringAsync();
+            }
+            if (statusCode == HttpStatusCode.Unauthorized)
+            {
+                return "Tiene que tener la sesión iniciada en el sistema";
+            }
+            if (statusCode == HttpStatusCode.Forbidden)
+            {
+                return "No tienes permiso para hacer esta operación.";
+            }
+
+            return "Ha ocurrido un errror inesperado";
+        }
+    }
+}
